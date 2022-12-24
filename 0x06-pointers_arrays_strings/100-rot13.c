7a64081bfde9881cd432f249ef33da2b63f91539
edem-okure <pcstoretech@yahoo.com>
2022-12-23 19:59:39 -0800

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,39 @@
 #include "main.h"
 /**
- * leet - encodes a string to 1337.
- * @str: the string to be encoded.
- * 
- * return: a pointer to the encoded string.
+ * rot13 - encodes a string using rot13.
+ * @str: string to be encoded.
+ *
+ * return: a pointer to the encoded sring.
  */
-char *leet(char *str)
+char *rot13(char *str)
 {
-	int indx1 = 0, indx2;
-
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int indx1 =0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'H', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
 	while (str[indx1])
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (str[indx1] == leet[indx2] ||
-					str[indx1] -32 == leet[indx2])
-				str[indx1] = indx2 + '0';
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
 		}
 		indx1++;
 	}
